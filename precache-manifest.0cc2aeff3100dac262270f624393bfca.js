fda530920769d7218741afc6369de37e496a9955
Megan McD <mcdevittbass@gmail.com>
2020-05-04 17:53:18 -0400

    Updates

---
+++ precache-manifest.0cc2aeff3100dac262270f624393bfca.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2710d434d94ed243f49b0bd539cd7092",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2cd0610503d0475c6b79",
+    "url": "/portfolio/static/css/2.e223cf29.chunk.css"
+  },
+  {
+    "revision": "a2521b17387feb2248a8",
+    "url": "/portfolio/static/css/main.23d379a7.chunk.css"
+  },
+  {
+    "revision": "2cd0610503d0475c6b79",
+    "url": "/portfolio/static/js/2.68d54021.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/portfolio/static/js/2.68d54021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2521b17387feb2248a8",
+    "url": "/portfolio/static/js/main.0da49c6b.chunk.js"
+  },
+  {
+    "revision": "ee3d72d161f98a5d4b9d",
+    "url": "/portfolio/static/js/runtime-main.f18b6ef5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
