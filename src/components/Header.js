9d7bb4a161b008a40fcfc6c2f93bc8c73f5a5f04
Megan McD <mcdevittbass@gmail.com>
2021-01-30 11:24:55 -0500

    Change resume and bio

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ export const Header = () => {
                 </Col>
                 <Col className=' col-6 col-sm-2 text-center'>
                     <Card className='header-icon justify-items-center'>
-                        <a role="button" href={baseUrl + '/img/McDevitt-Resume-Jan-2021.pdf'} target="_blank" rel="noopener noreferrer" title="Resume">
+                        <a role="button" href={baseUrl + '/img/McDevitt-Resume-Feb-2021.pdf'} target="_blank" rel="noopener noreferrer" title="Resume">
                             <i className="fa fa-file-pdf-o fa-5x fa-inverse"></i>
                         </a>
                         <CardText className='text-center nowrap'>Resume</CardText>
